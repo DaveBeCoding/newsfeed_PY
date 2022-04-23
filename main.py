b2ff3522b673af954299c4471d3b7479f69505aa
davebecoding <d01001010@csu.fullerton.edu>
2022-04-23 09:16:51 -0700

    file structure

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import scrapes
-
+# import scrapes
+from scrapes import Scraper
 
 def main():
     print("Hello Test scraper")
-    scrapes.Scraper("john").say_hi()
+    # scrapes.Scraper("john").say_hi()
+    Scraper("john").say_hi()
 
 
 main()
